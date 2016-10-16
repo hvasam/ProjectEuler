40da278b522792d308ee935baa5213abed2dcede
Harshavardhan Vasam <ls_players@hotmail.com>
2016-10-16 18:53:14 -0400

    Problem 7 Solution

--- problem7.cpp
+++ problem7.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 
+bool isPrime(int n) { // Miller-Rabin Test is an alternative for a faster run time 
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int nthPrime(int n) {
+	int primeCount = 0;
+	int nthPrime = 2;
+	while (primeCount < n) {
+		if (isPrime(nthPrime)) {
+			++primeCount;
+		}
+		++nthPrime;
+	}
+	return nthPrime - 1;
+}
+
+int main() {
+	std::cout << nthPrime(10001) << std::endl;
+}
